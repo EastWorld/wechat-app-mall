42032bfd0266033491fa99e27465f9049e98bf1f
gooking <javazj@qq.com>
2017-08-25 10:42:32 +0800

    在 我的 里面增加了绑定微信认证的手机号码功能

--- pages/my/index.js
+++ pages/my/index.js
@@ -2,13 +2,14 @@ const app = getApp()
 
 Page({
 	data: {
-		userInfo: {},		
+		userInfo: {}
 	},
 	onLoad() {
     this.getUserInfo();
     this.setData({
       version: app.globalData.version
     });
+    this.getUserApiInfo ();
 	},	
   getUserInfo: function (cb) {
       var that = this
@@ -30,5 +31,58 @@ Page({
       content: '本系统基于开源小程序商城系统 https://github.com/EastWorld/wechat-app-mall 搭建，祝大家使用愉快！',
       showCancel:false
     })
+  },
+  getPhoneNumber: function(e) {
+    if (!e.detail.errMsg || e.detail.errMsg != "getPhoneNumber:ok") {
+      wx.showModal({
+        title: '提示',
+        content: '无法获取手机号码',
+        showCancel: false
+      })
+      return;
+    }
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/user/wxapp/bindMobile',
+      data: {
+        token: app.globalData.token,
+        encryptedData: e.detail.encryptedData,
+        iv: e.detail.iv
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          wx.showToast({
+            title: '绑定成功',
+            icon: 'success',
+            duration: 2000
+          })
+          that.getUserApiInfo();
+        } else {
+          wx.showModal({
+            title: '提示',
+            content: '绑定失败',
+            showCancel: false
+          })
+        }
+      }
+    })
+  },
+  getUserApiInfo: function () {
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/user/detail',
+      data: {
+        token: app.globalData.token
+      },
+      success: function (res) {
+        if (res.data.code == 0) {
+          that.setData({
+            apiUserInfoMap: res.data.data,
+            userMobile: res.data.data.base.mobile
+          });
+        }
+      }
+    })
+
   }
 })

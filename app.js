5ef45623ee3971b7ccaceb85ab32bd3ffd680e75
gooking <javazj@qq.com>
2018-01-11 17:05:42 +0800

    增加梦幻引导页面，@_干将~ 贡献代码

--- app.js
+++ app.js
@@ -114,6 +114,25 @@ App({
       }
     })
   },
+  getUserInfo: function (cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登陆接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+
+  },
   globalData:{
     userInfo:null,
     subDomain: "tz", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd

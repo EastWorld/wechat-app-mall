53c794b742a0169defc5d125f75da95a8f5cd93e
liuzhenjun <>
2018-04-21 14:50:29 +0800

    修改初始信息

--- pages/my/index.js
+++ pages/my/index.js
@@ -36,7 +36,7 @@ Page({
   aboutUs : function () {
     wx.showModal({
       title: '关于我们',
-      content: '本系统基于开源小程序商城系统 https://github.com/EastWorld/wechat-app-mall 搭建，祝大家使用愉快！',
+      content: '地址：长沙市雨花区左家塘街道车站南路4号；电话：13203119387',
       showCancel:false
     })
   },

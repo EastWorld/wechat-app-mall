6b368dae4ccc4523c0b93fcf1d6707ef08b7e4e4
gooking <javazj@qq.com>
2018-05-30 07:05:35 +0800

    app.js 增加了check-token判断，避免无法登录的问题

--- app.js
+++ app.js
@@ -2,6 +2,24 @@
 App({
   onLaunch: function () {
     var that = this;
+    // 判断是否登录
+    let token = wx.getStorageSync('token');
+    if (!token){
+      that.goLoginPageTimeOut()
+      return
+    }
+    wx.request({
+      url: 'https://api.it120.cc/' + that.globalData.subDomain + '/user/check-token',
+      data: {
+        token: token
+      },
+      success: function (res) {
+        if (res.data.code != 0) {
+          wx.removeStorageSync('token')
+          that.goLoginPageTimeOut()
+        }
+      }
+    })
     //  获取商城名称
     wx.request({
       url: 'https://api.it120.cc/'+ that.globalData.subDomain +'/config/get-value',
@@ -95,6 +113,13 @@ App({
       }
     })
   },  
+  goLoginPageTimeOut: function () {
+    setTimeout(function(){
+      wx.navigateTo({
+        url: "/pages/authorize/index"
+      })
+    }, 1000)    
+  },
   globalData:{
     userInfo:null,
     subDomain: "tz", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd

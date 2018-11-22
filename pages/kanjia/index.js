729294fa9af75775da3eb6b8586d68264b11595a
gooking <javazj@qq.com>
2018-11-22 13:25:22 +0800

    fix

--- pages/kanjia/index.js
+++ pages/kanjia/index.js
@@ -19,29 +19,33 @@ Page({
         swiperCurrent: e.detail.current  
     })  
   },
-  onLoad: function (e) {    
+  onLoad: function (e) {
     this.data.id = e.id;
     this.data.kjId = e.kjId;
     this.data.joiner = e.joiner;
-    var that = this;
+  },
+  onShow: function () {
+    var that = this
     wx.request({
-      url: 'https://api.it120.cc/'+ app.globalData.subDomain +'/shop/goods/detail',
+      url: 'https://api.it120.cc/' + app.globalData.subDomain + '/shop/goods/detail',
       data: {
-        id: e.id
+        id: that.data.id
       },
-      success: function(res) {
-        that.data.goodsDetail = res.data.data;
-        if (res.data.data.basicInfo.videoId) {
-          that.getVideoSrc(res.data.data.basicInfo.videoId);
+      success: function (res) {
+        if (res.code == 0) {
+          that.data.goodsDetail = res.data.data;
+          if (res.data.data.basicInfo.videoId) {
+            that.getVideoSrc(res.data.data.basicInfo.videoId);
+          }
+          that.setData({
+            goodsDetail: res.data.data
+          });
+          WxParse.wxParse('article', 'html', res.data.data.content, that, 5);
+          that.getKanjiaInfo(that.data.kjId, that.data.joiner);
+          that.getKanjiaInfoMyHelp(that.data.kjId, that.data.joiner);
         }
-        that.setData({
-          goodsDetail:res.data.data
-        });
-        WxParse.wxParse('article', 'html', res.data.data.content, that, 5);
       }
     })
-    this.getKanjiaInfo(e.kjId, e.joiner);
-    this.getKanjiaInfoMyHelp(e.kjId, e.joiner);
   },
   onShareAppMessage: function () {
     var that = this;

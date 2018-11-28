370a000acb49faec8711021bc9e0beba5c26d2ec
gooking <javazj@qq.com>
2018-11-28 16:00:46 +0800

    增加三级分销注册支持：商品转发给好友，好友通过链接进来后完成新用户注册，即可奖励三级分销用户关系

--- pages/authorize/index.js
+++ pages/authorize/index.js
@@ -132,18 +132,23 @@ Page({
     })
   },
   registerUser: function () {
-    var that = this;
+    let that = this;
     wx.login({
       success: function (res) {
-        var code = res.code; // 微信登录接口返回的 code 参数，下面注册接口需要用到
+        let code = res.code; // 微信登录接口返回的 code 参数，下面注册接口需要用到
         wx.getUserInfo({
           success: function (res) {
-            var iv = res.iv;
-            var encryptedData = res.encryptedData;
+            let iv = res.iv;
+            let encryptedData = res.encryptedData;
+            let referrer = '' // 推荐人
+            let referrer_storge = wx.getStorageSync('referrer');
+            if (referrer_storge) {
+              referrer = referrer_storge;
+            }
             // 下面开始调用注册接口
             wx.request({
               url: 'https://api.it120.cc/' + app.globalData.subDomain + '/user/wxapp/register/complex',
-              data: { code: code, encryptedData: encryptedData, iv: iv }, // 设置请求的 参数
+              data: { code: code, encryptedData: encryptedData, iv: iv, referrer: referrer }, // 设置请求的 参数
               success: (res) => {
                 wx.hideLoading();
                 that.login();

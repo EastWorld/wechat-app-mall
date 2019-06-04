a9b82b361998b6b376f2df355cc29b011e2759f5
gooking <javazj@qq.com>
2019-06-04 16:10:02 +0800

    修复订单列表的商品数量一直显示为1的问题

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  version: "6.7.0",
-  note: '增加在线申请退换货逻辑',
+  version: "6.7.1",
+  note: '修复订单列表的商品数量一直显示为1的问题',
   subDomain: "tz", // https://admin.it120.cc 登录后台首页的专属域名
   appid: "wxa46b09d413fbcaff", // 您的小程序的appid，购物单功能需要使用
   shareProfile: '百款精品商品，总有一款适合您' // 首页转发的时候话术

e11433e66b7a33db31306f6ee3f1b8ac5b4c1e5b
Simon <hulucode@163.com>
2019-11-16 18:24:06 +0800

    优化分类内容的样式并且开启了分类功能

--- pages/category/category.js
+++ pages/category/category.js
@@ -3,108 +3,71 @@
 const WXAPI = require('apifm-wxapi')
 
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     categories: [],
-    goodsWrap: [],
-    categorySelected: "",
-    goodsToView: "",
-    categoryToView: "",
+    categorySelected: {
+      name: '',
+      id: ''
+    },
+    currentGoods: [],
+    onLoadStatus: true,
+    scrolltop: 0
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
     this.initData();
   },
   initData() {
-
     let that = this;
     wx.showNavigationBarLoading();
     WXAPI.goodsCategory().then(function(res) {
-
       var categories = [];
+      var categoryName = '';
+      var categoryId = '';
       if (res.code == 0) {
         for (var i = 0; i < res.data.length; i++) {
-
           let item = res.data[i];
-
-          item.scrollId = "s" + item.id;
           categories.push(item);
-
           if (i == 0) {
-
-            that.setData({
-              categorySelected: item.scrollId,
-            })
-
+            categoryName = item.name;
+            categoryId = item.id;
           }
         }
       }
       that.setData({
         categories: categories,
-
+        categorySelected: {
+          name: categoryName,
+          id: categoryId
+        }
       });
-      console.log(categories);
-      that.getGoodsList(0);
+      // console.log(categories);
+      that.getGoodsList();
     }).catch((e) => {
-
       wx.hideNavigationBarLoading();
     });
-
   },
-  getGoodsList: function(categoryId, append) {
-
+  getGoodsList: function() {
     let that = this;
-
     WXAPI.goods({
-      categoryId: "",
+      categoryId: that.data.categorySelected.id,
       page: 1,
       pageSize: 100000
     }).then(function(res) {
       if (res.code == 404 || res.code == 700) {
-
         return
       }
-      let goodsWrap = [];
-
-
-      that.data.categories.forEach((o, index) => {
-
-        let wrap = {};
-        wrap.id = o.id;
-        wrap.scrollId = "s" + o.id;
-        wrap.name = o.name;
-        let goods = [];
-
-        wrap.goods = goods;
-        res.data.forEach((item, i) => {
-
-          if (item.categoryId == wrap.id) {
-
-            goods.push(item)
-          }
-        })
-
-        goodsWrap.push(wrap);
-      })
-
-
-
       that.setData({
-        goodsWrap: goodsWrap,
+        currentGoods: res.data
       });
-
-      console.log(goodsWrap);
-
+      console.log(res.data);
       wx.hideNavigationBarLoading();
     }).catch((e) => {
-
       wx.hideNavigationBarLoading();
     });
   },
@@ -114,73 +77,29 @@ Page({
     })
   },
   onCategoryClick: function(e) {
-
-    let id = e.currentTarget.dataset.id;
-    this.categoryClick = true;
-    this.setData({
-      goodsToView: id,
-      categorySelected: id,
-    })
-
-  },
-  scroll: function(e) {
-
-    if (this.categoryClick){
-      this.categoryClick = false;
-      return;
-    }
-
-    let scrollTop = e.detail.scrollTop;
-
-    let that = this;
-
-    let offset = 0;
-    let isBreak = false;
-
-    for (let g = 0; g < this.data.goodsWrap.length; g++) {
-
-      let goodWrap = this.data.goodsWrap[g];
-
-      offset += 30;
-
-      if (scrollTop <= offset) {
-
-        if (this.data.categoryToView != goodWrap.scrollId) {
-          this.setData({
-            categorySelected: goodWrap.scrollId,
-            categoryToView: goodWrap.scrollId,
-          })
-        }
-
-        break;
-      }
-
-
-      for (let i = 0; i < goodWrap.goods.length; i++) {
-
-        offset += 91;
-
-        if (scrollTop <= offset) {
-
-          if (this.data.categoryToView != goodWrap.scrollId) {
-            this.setData({
-              categorySelected: goodWrap.scrollId,
-              categoryToView: goodWrap.scrollId,
-            })
-          }
-
-          isBreak = true;
+    var that = this;
+    var id = e.target.dataset.id;
+    if (id === that.data.categorySelected.id) {
+      that.setData({
+        scrolltop: 0,
+      })
+    } else {
+      var categoryName = '';
+      for (var i = 0; i < that.data.categories.length; i++) {
+        let item = that.data.categories[i];
+        if (item.id == id) {
+          categoryName = item.name;
           break;
         }
       }
-
-      if (isBreak){
-        break;
-      }
-
-
+      that.setData({
+        categorySelected: {
+          name: categoryName,
+          id: id
+        },
+        scrolltop: 0
+      });
+      that.getGoodsList();
     }
-
-  
   }
 })

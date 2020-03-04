169e5ad9457bba7aaa23cf035a1cea4c01bcb70f
gooking <javazj@qq.com>
2020-03-05 01:17:19 +0800

    我的订单增加全部类型

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  version: "7.13.4",
-  note: 'fix砍价后自己发起砍价无法给自己砍的问题', // 这个为版本描述，无需修改
+  version: "7.14.1",
+  note: '我的订单增加全部类型', // 这个为版本描述，无需修改
   subDomain: "tz", // 根据教程 https://www.yuque.com/apifm/doc/qr6l4m 查看你自己的 subDomain
   shareProfile: '百款精品商品，总有一款适合您', // 首页转发的时候话术
   goodsDetailSkuShowType: 0, // 0 为点击立即购买按钮后出现规格尺寸、数量的选择； 1为直接在商品详情页面显示规格尺寸、数量的选择，而不弹框
--- pages/order-list/index.js
+++ pages/order-list/index.js
@@ -5,16 +5,36 @@ const AUTH = require('../../utils/auth')
 
 Page({
   data: {
-    statusType: ["待付款", "待发货", "待收货", "待评价", "已完成"],
+    statusType: [
+      {
+        status: 9999,
+        label: '全部'
+      },
+      {
+        status: 0,
+        label: '待付款'
+      },
+      {
+        status: 1,
+        label: '待发货'
+      },
+      {
+        status: 2,
+        label: '待收货'
+      },
+      {
+        status: 3,
+        label: '待评价'
+      },
+    ],
+    status: 9999,
     hasRefund: false,
-    currentType: 0,
-    tabClass: ["", "", "", "", ""]
+    badges: [0, 0, 0, 0, 0]
   },
   statusTap: function(e) {
-    const curType = e.currentTarget.dataset.index;
-    this.data.currentType = curType
+    const status = e.currentTarget.dataset.status;
     this.setData({
-      currentType: curType
+      status
     });
     this.onShow();
   },
@@ -120,13 +140,12 @@ Page({
     if (options && options.type) {
       if (options.type == 99) {
         this.setData({
-          hasRefund: true,
-          currentType: options.type
+          hasRefund: true
         });
       } else {
         this.setData({
           hasRefund: false,
-          currentType: options.type
+          status: options.type
         });
       }      
     }
@@ -135,40 +154,26 @@ Page({
     // 生命周期函数--监听页面初次渲染完成
 
   },
-  getOrderStatistics: function() {
-    var that = this;
-    WXAPI.orderStatistics(wx.getStorageSync('token')).then(function(res) {
+  getOrderStatistics() {
+    WXAPI.orderStatistics(wx.getStorageSync('token')).then(res => {
       if (res.code == 0) {
-        var tabClass = that.data.tabClass;
+        const badges = this.data.badges;
         if (res.data.count_id_no_pay > 0) {
-          tabClass[0] = "red-dot"
-        } else {
-          tabClass[0] = ""
+          badges[1] = res.data.count_id_no_pay
         }
         if (res.data.count_id_no_transfer > 0) {
-          tabClass[1] = "red-dot"
-        } else {
-          tabClass[1] = ""
+          badges[2] = res.data.count_id_no_transfer
         }
         if (res.data.count_id_no_confirm > 0) {
-          tabClass[2] = "red-dot"
-        } else {
-          tabClass[2] = ""
+          badges[3] = res.data.count_id_no_confirm
         }
         if (res.data.count_id_no_reputation > 0) {
-          tabClass[3] = "red-dot"
-        } else {
-          tabClass[3] = ""
-        }
-        if (res.data.count_id_success > 0) {
-          //tabClass[4] = "red-dot"
-        } else {
-          //tabClass[4] = ""
+          badges[4] = res.data.count_id_no_reputation
         }
 
-        that.setData({
-          tabClass: tabClass,
-        });
+        this.setData({
+          badges
+        })
       }
     })
   },
@@ -195,7 +200,7 @@ Page({
       }
     })
   },
-  doneShow: function() {
+  doneShow() {
     // 获取订单列表
     var that = this;
     var postData = {
@@ -203,7 +208,10 @@ Page({
     };
     postData.hasRefund = that.data.hasRefund;
     if (!postData.hasRefund) {
-      postData.status = that.data.currentType;
+      postData.status = that.data.status;
+    }
+    if (postData.status == 9999) {
+      postData.status = ''
     }
     this.getOrderStatistics();
     WXAPI.orderList(postData).then(function(res) {

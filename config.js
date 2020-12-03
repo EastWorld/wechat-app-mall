520380cac0398e9025b983f2aef2ca5bc871a76e
gooking <javazj@qq.com>
2020-12-03 10:06:29 +0800

    下单选择自提的话，需要填写联系人和联系电话

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: '9.4.0',
-  note: '申请开票界面增加查询按钮', // 这个为版本描述，无需修改
+  version: '9.5.0',
+  note: '下单选择自提的话，需要填写联系人和联系电话', // 这个为版本描述，无需修改
   subDomain: 'tz', // 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
 }
--- pages/to-pay-order/index.js
+++ pages/to-pay-order/index.js
@@ -187,17 +187,30 @@ Page({
         })
         return;
       }
-      if(postData.peisongType == 'zq' && !this.data.mobile) {
-        wx.showToast({
-          title: '请填写手机号码',
-          icon: 'none'
-        })
-        return;
+      const extJsonStr = {}
+      if(postData.peisongType == 'zq') {
+        if(!this.data.name) {
+          wx.showToast({
+            title: '请填写联系人',
+            icon: 'none'
+          })
+          return;
+        }
+        if(!this.data.mobile) {
+          wx.showToast({
+            title: '请填写联系电话',
+            icon: 'none'
+          })
+          return;
+        }
+        extJsonStr['联系人'] = this.data.name
+        extJsonStr['联系电话'] = this.data.mobile
       }
       if(postData.peisongType == 'zq' && this.data.shops) {
         postData.shopIdZt = this.data.shops[this.data.shopIndex].id
         postData.shopNameZt = this.data.shops[this.data.shopIndex].name
       }
+      postData.extJsonStr = JSON.stringify(extJsonStr)
     }
 
     WXAPI.orderCreate(postData).then(function (res) {

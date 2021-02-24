056add309e6ad1fe283407dfa694952f2a583fe6
gooking <javazj@qq.com>
2021-02-24 09:53:22 +0800

    瘦身代码包

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  version: '10.5.0',
-  note: '分类页面定位bug，分销团队页面优化，购物车红点位置bug', // 这个为版本描述，无需修改
+  version: '10.5.1',
+  note: '瘦身代码包', // 这个为版本描述，无需修改
   subDomain: 'tz', // 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
   sdkAppID: 1400450467, // 腾讯实时音视频应用编号，请看教程 https://www.it120.cc/help/nxoqsl.html
 }

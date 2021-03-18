77929f0ef03c53025b87bc615b6bd7249ec586bf
gooking <javazj@qq.com>
2021-03-18 09:41:10 +0800

    分类页面增加左侧宽度，右上子分类不省略过长的分类名称

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  version: '11.0.0',
-  note: '界面优化', // 这个为版本描述，无需修改
-  subDomain: 'tz', // 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
+  version: '11.0.1',
+  note: '分类页面增加左侧宽度，右上子分类不省略过长的分类名称', // 这个为版本描述，无需修改
+  subDomain: 'tz', // liu123 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
   merchantId: 951, // 商户ID，可在后台工厂设置-->商户信息查看
   sdkAppID: 1400450467, // 腾讯实时音视频应用编号，请看教程 https://www.it120.cc/help/nxoqsl.html
 }

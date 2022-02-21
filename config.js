40667a6d41baddb72791c082e17403c43c9211d4
gooking <javazj@qq.com>
2022-02-21 23:47:14 +0800

    商品详情的评价显示不齐全 #279

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  version: '12.12.1',
-  note: '商品详情界面点击tab会跳到下一个tab #278', // 这个为版本描述，无需修改
+  version: '12.12.2',
+  note: '商品详情的评价显示不齐全 #279', // 这个为版本描述，无需修改
   subDomain: 'tz', // liu123 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
   merchantId: 951, // 商户ID，可在后台工厂设置-->商户信息查看
   sdkAppID: 1400450467, // 腾讯实时音视频应用编号，请看教程 https://www.it120.cc/help/nxoqsl.html

a43971bd1df4a69249ad54e7cdca0b50cdb1e014
gooking <javazj@qq.com>
2024-10-18 07:48:07 +0800

    商品详情增加显示购买须知和商品特色

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  version: '24.10.15',
-  note: 'banner链接跳转bug fixed', // 这个为版本描述，无需修改
+  version: '24.10.18',
+  note: '商品详情增加显示购买须知和商品特色', // 这个为版本描述，无需修改
   subDomain: 'tz', // 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
   merchantId: 951, // 商户ID，可在后台工厂设置-->商户信息查看
   sdkAppID: 1400450467, // 腾讯实时音视频应用编号，请看教程 https://www.it120.cc/help/nxoqsl.html

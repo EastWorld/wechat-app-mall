bbdd07669ea3215cd2c346787db1e0d2fb58713d
gooking <javazj@qq.com>
2024-10-25 22:38:55 +0800

    登陆注册流程代码优化

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ module.exports = {
   sdkAppID: 1400450467, // 腾讯实时音视频应用编号，请看教程 https://www.it120.cc/help/nxoqsl.html
   bindSeller: false, // true 开启三级分销抢客； false 为不开启
   customerServiceType: 'QW', // 客服类型，QW为企业微信，需要在后台系统参数配置企业ID和客服URL，XCX 为小程序的默认客服
-  openIdAutoRegister: false, // 用户打开小程序的时候自动注册新用户【用户不存在的时候】
+  openIdAutoRegister: true, // 用户打开小程序的时候自动注册新用户【用户不存在的时候】
 }
